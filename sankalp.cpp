2daf7852ed4e635e4dbd14d83b4a1ba282f9b439
Sankalp Kulkarni <79413990+sankalpkulk06@users.noreply.github.com>
2023-02-19 21:41:39 +0530

    Update and rename hello.cpp to sankalp.cpp

--- sankalp.cpp
+++ sankalp.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "Name: Sankalp Kulkarni\nSRN: PES1UG20CS384";
+  cout << "Name: Sankalp Kulkarni\nSRN: PES1UG20CS384\n";
+  cout << "Task-5";
   return 0;
 }
